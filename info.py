cfc3eb2067ccecb36537686f83e77fe6c20943c4
movietime9988 <drjain305005@gmail.com>
2025-07-07 09:33:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Govieed:Govieed@goviee
 DATABASE_NAME = environ.get("DATABASE_NAME", "Govieed")
 
 # Files index database url
-FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://Govieed:<db_password>@govieed.vg291mc.mongodb.net/?retryWrites=true&w=majority&appName=Govieed")
+FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://Chovieed:Chovieed@chovieed.a5h6btq.mongodb.net/?retryWrites=true&w=majority&appName=Chovieed")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "jisshu")
 
 # Other Channel's
